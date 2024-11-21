62531f942e81494f7ae7d8f69967802ec7afff3c
ALI ZULFIQAR <alimalikali1928@gmail.com>
2024-11-21 13:10:14 +0500

    sever issue fixed

--- server/constants/config.js
+++ server/constants/config.js
@@ -1,7 +1,7 @@
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "http://localhost:4173",
+    "https://devali-chatapp.vercel.app",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
